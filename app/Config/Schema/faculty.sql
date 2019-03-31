8f051141149be47f85d9740f57ac235663e65599
obamenandro <obamenandro@gmail.com>
2019-03-31 23:14:41 +0800

    develop

--- app/Config/Schema/faculty.sql
+++ app/Config/Schema/faculty.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Mar 25, 2019 at 01:33 PM
+-- Generation Time: Mar 31, 2019 at 03:13 PM
 -- Server version: 5.7.14
 -- PHP Version: 7.0.10
 
@@ -59,6 +59,13 @@ CREATE TABLE `faculties` (
   `modified` datetime DEFAULT NULL COMMENT 'Modified Date'
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
+--
+-- Dumping data for table `faculties`
+--
+
+INSERT INTO `faculties` (`id`, `firstname`, `middlename`, `lastname`, `email`, `username`, `password`, `birthday`, `date_hired`, `address`, `gender`, `contact`, `deleted_date`, `created`, `modified`) VALUES
+(1, 'menandro', '', 'oba', 'obamenandro@gmail.com', 'obamenandro', 'd033e22ae348aeb5660fc2140aec35850c4da997', '03/05/2019', '03/26/2019', '1076 Tensuan Site Poblacion', 1, 9154386367, NULL, '2019-03-31 13:46:21', '2019-03-31 13:46:21');
+
 -- --------------------------------------------------------
 
 --
@@ -95,7 +102,42 @@ CREATE TABLE `faculty_subjects` (
 
 INSERT INTO `faculty_subjects` (`id`, `faculty_id`, `subject_id`, `deleted_date`, `created`, `modified`) VALUES
 (6, 8, 4, NULL, '2019-03-24 11:31:03', '2019-03-24 11:31:03'),
-(7, 8, 5, NULL, '2019-03-24 11:31:03', '2019-03-24 11:31:03');
+(7, 8, 5, NULL, '2019-03-24 11:31:03', '2019-03-24 11:31:03'),
+(8, 1, 1, NULL, '2019-03-31 13:46:21', '2019-03-31 13:46:21'),
+(9, 1, 2, NULL, '2019-03-31 13:46:21', '2019-03-31 13:46:21');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `grades`
+--
+
+CREATE TABLE `grades` (
+  `id` int(11) NOT NULL,
+  `deleted_date` datetime DEFAULT NULL,
+  `student_id` int(11) NOT NULL,
+  `first` int(11) DEFAULT '0',
+  `second` int(11) DEFAULT '0',
+  `third` int(11) DEFAULT '0',
+  `fourth` int(11) DEFAULT '0',
+  `final` int(11) DEFAULT '0',
+  `grade_id` int(11) NOT NULL DEFAULT '0',
+  `subject_id` int(11) DEFAULT '0',
+  `created` datetime DEFAULT NULL COMMENT 'Created Date',
+  `modified` datetime DEFAULT NULL COMMENT 'Modified Date'
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+--
+-- Dumping data for table `grades`
+--
+
+INSERT INTO `grades` (`id`, `deleted_date`, `student_id`, `first`, `second`, `third`, `fourth`, `final`, `grade_id`, `subject_id`, `created`, `modified`) VALUES
+(21, NULL, 1, 90, 90, 90, 90, 90, 10, 1, '2019-03-31 13:03:51', '2019-03-31 13:03:51'),
+(22, NULL, 1, 90, 90, 90, 90, 90, 10, 2, '2019-03-31 13:03:51', '2019-03-31 13:03:51'),
+(23, NULL, 1, 90, 90, 90, 90, 90, 10, 3, '2019-03-31 13:03:51', '2019-03-31 13:03:51'),
+(24, NULL, 1, 90, 90, 90, 90, 90, 10, 4, '2019-03-31 13:03:51', '2019-03-31 13:03:51'),
+(25, NULL, 1, 90, 90, 100, 90, 93, 10, 5, '2019-03-31 13:03:51', '2019-03-31 13:03:51'),
+(26, NULL, 1, 90, 90, 100, 90, 93, 9, 5, '2019-03-31 13:03:51', '2019-03-31 13:03:51');
 
 -- --------------------------------------------------------
 
@@ -197,6 +239,12 @@ ALTER TABLE `faculty_subjects`
   ADD PRIMARY KEY (`id`);
 
 --
+-- Indexes for table `grades`
+--
+ALTER TABLE `grades`
+  ADD PRIMARY KEY (`id`);
+
+--
 -- Indexes for table `students`
 --
 ALTER TABLE `students`
@@ -227,7 +275,7 @@ ALTER TABLE `categories`
 -- AUTO_INCREMENT for table `faculties`
 --
 ALTER TABLE `faculties`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
 --
 -- AUTO_INCREMENT for table `faculty_students`
 --
@@ -237,7 +285,12 @@ ALTER TABLE `faculty_students`
 -- AUTO_INCREMENT for table `faculty_subjects`
 --
 ALTER TABLE `faculty_subjects`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=8;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=10;
+--
+-- AUTO_INCREMENT for table `grades`
+--
+ALTER TABLE `grades`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=27;
 --
 -- AUTO_INCREMENT for table `students`
 --
